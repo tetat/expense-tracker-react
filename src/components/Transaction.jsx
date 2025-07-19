4ebb268d29de90ff09bab806b0d3b4ad725a90ec
Md Nishat Miah <nishat00101@gmail.com>
2025-07-20 00:18:07 +0600

    Transaction delete added

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,11 +1,21 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
 const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
   const sign = transaction.amount < 0 ? "-" : "+";
 
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text + " "}
       <span>{sign + "৳" + Math.abs(transaction.amount)}</span>
-      <button className="delete-btn">x</button>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
     </li>
   );
 };
