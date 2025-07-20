f8aa152a4ed7b8ba38e4581f7d670f7e1b205993
Md Nishat Miah <nishat00101@gmail.com>
2025-07-21 01:10:58 +0600

    Add Transaction added

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -8,11 +8,15 @@ const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
-      <ul className="list">
-        {transactions.map((transaction) => {
-          return <Transaction key={transaction.id} transaction={transaction} />;
-        })}
-      </ul>
+      {transactions.length === 0 ? (
+        <p className="no-transactions">No transactions yet.</p>
+      ) : (
+        <ul className="list">
+          {transactions.map((transaction) => (
+            <Transaction key={transaction.id} transaction={transaction} />
+          ))}
+        </ul>
+      )}
     </>
   );
 };
